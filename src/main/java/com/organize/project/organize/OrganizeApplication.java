eb77a5ec8a5aeea3efd637b036dcd6cbd7cdcf55
ktatamonem <monemktata@gmail.com>
2017-12-31 15:33:33 +0100

    test

---
+++ src/main/java/com/organize/project/organize/OrganizeApplication.java
@@ -0,0 +1,12 @@
+package com.organize.project.organize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizeApplication.class, args);
+	}
+}
